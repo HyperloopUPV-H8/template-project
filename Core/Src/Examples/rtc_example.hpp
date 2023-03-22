0f86b43a5d1516372e503258cef3a171f458e37b
ricardo chust <ricardochustfides@gmail.com>
2023-03-22 15:09:48 +0100

    sntp working on rtc example

--- Core/Src/Examples/rtc_example.hpp
+++ Core/Src/Examples/rtc_example.hpp
@@ -6,18 +6,22 @@
  */
 
 #pragma once
-
+#include "main.h"
+#include "lwip.h"
 #include "ST-LIB.hpp"
 
 int rtc_example(void){
+	STLIB::start(Nucleo,"192.168.1.4","255.255.0.0","192.168.1.1");
+	uint32_t a = HAL_RCC_GetSysClockFreq();
+	printf("%lu",a);
 
-	STLIB::start(Nucleo);
+	SNTP::sntp_update("192.168.1.3");
 
-	//Time::set_RTC_data(0,30,20,10,28,2,2023);
 	printf("\n\n\n\n\n\n\n\n\n\n");
 	while(1){
-		//Time::rtc_data now = Time::get_RTC_data();
-		//printf("%d:%d:%d::%d  %d-%d-%d \n",now.hour,now.minute,now.second,now.counter,now.day,now.month,now.year);
-		HAL_Delay(100);
+		STLIB::update();
+		Time::RTCData now = Time::get_rtc_data();
+		printf("%u:%u:%u::%u  %u-%u-%u \n",now.hour,now.minute,now.second,now.counter,now.day,now.month,now.year);
 	}
 }
+
