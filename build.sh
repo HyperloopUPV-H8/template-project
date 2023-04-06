6e9601e820749c313a54b8e8e4e9489b2e10e3d9
alejandrogonzalvo <alejandrogonhid@protonmail.com>
2023-04-06 09:39:08 +0200

    update build

--- build.sh
+++ build.sh
@@ -7,6 +7,13 @@ start=`date +%s.%N`
 
 rm -R CMakeCache.txt compile_commands.json cmake_install.cmake Makefile CMakeFiles
 
+if [[ $* == *--NOETH* ]]
+then
+  ETH="NOETH"
+else
+  ETH="HAL_ETH_MODULE_ENABLED"
+fi
+
 if ([ $TARGET = "board" ] || [ $TARGET = "b" ] || [ $TARGET = "B" ] || [ $TARGET = "Board" ] || [ $TARGET = "BOARD" ])
 then
   TARGET="BOARD"
@@ -20,18 +27,24 @@ else
   fi
 fi
 
-cmake -DCMAKE_TOOLCHAIN_FILE=arm-none-eabi.cmake -D${TARGET}=ON .
+cmake -DCMAKE_TOOLCHAIN_FILE=arm-none-eabi.cmake -D${TARGET}=ON -D${ETH}=ON .
 make -j16 all
 
 end=`date +%s.%N`
 runtime=$( echo "$end - $start" | bc -l )
 
-RED='\033[0;31m'
-YELLOW='\033[0;33m'
+RED='\033[1;31m'
+YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 EXECUTABLE=template-project.elf
 PROJECT_NAME=template-project
-printf "\n\n\n${RED}Template project${NC} built for ${YELLOW}${TARGET}${NC} in ${YELLOW}$runtime ${NC}seconds!\n\n\n"
+
+
+printf "\n\n\n${RED}%25s${NC} built!\n\n" ${PROJECT_NAME}
+
+printf "${RED}%20s ${NC}: ${YELLOW}\t%s\n" Target ${TARGET}
+printf "${RED}%20s ${NC}: ${YELLOW}\t%ss\n" Compile $runtime
+printf "\n"
 arm-none-eabi-objcopy -O ihex ${EXECUTABLE} ${PROJECT_NAME}.hex
 arm-none-eabi-objcopy -O binary ${EXECUTABLE} ${PROJECT_NAME}.bin
 
