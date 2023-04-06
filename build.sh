7d77c60d28f6bb3b46af74b368a00f9d7bbb1d3a
alejandrogonzalvo <alejandrogonhid@protonmail.com>
2023-04-06 08:39:45 +0200

    prettify build script

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 echo "Enter Target (nucleo|n / board|b) : "
 read TARGET
 
+start=`date +%s.%N`
+
 rm -R CMakeCache.txt compile_commands.json cmake_install.cmake Makefile CMakeFiles
 
 if ([ $TARGET = "board" ] || [ $TARGET = "b" ] || [ $TARGET = "B" ] || [ $TARGET = "Board" ] || [ $TARGET = "BOARD" ])
@@ -21,12 +23,15 @@ fi
 cmake -DCMAKE_TOOLCHAIN_FILE=arm-none-eabi.cmake -D${TARGET}=ON .
 make -j16 all
 
+end=`date +%s.%N`
+runtime=$( echo "$end - $start" | bc -l )
+
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 EXECUTABLE=template-project.elf
 PROJECT_NAME=template-project
-printf "\n\n\n${RED}Template project${NC} built for ${YELLOW}${TARGET}\n\n\n"
+printf "\n\n\n${RED}Template project${NC} built for ${YELLOW}${TARGET}${NC} in ${YELLOW}$runtime ${NC}seconds!\n\n\n"
 arm-none-eabi-objcopy -O ihex ${EXECUTABLE} ${PROJECT_NAME}.hex
 arm-none-eabi-objcopy -O binary ${EXECUTABLE} ${PROJECT_NAME}.bin
 
