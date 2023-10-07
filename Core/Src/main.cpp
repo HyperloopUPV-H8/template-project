1ea188627d41ef94188915dc9e9964f42421f00b
Gonzalo <gonzalosmoya24@gmail.com>
2023-10-07 20:11:34 +0200

    updated build tools

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -4,11 +4,49 @@
 #include "ST-LIB.hpp"
 #include "Runes/Runes.hpp"
 
+using namespace std::chrono_literals;
+
+enum States{
+	OPERATIONAL,
+	FALLO
+};
+
+class Test{
+public:
+	uint64_t current = 0;
+	void add(){
+		add_protection(&current,Boundary<uint64_t,BELOW>(10));
+	}
+
+};
+
+
 int main(void)
 {
+
+	//uint64_t current = 0;
+	Test test;
+	StateMachine state_machine;
+	state_machine.add_state(States::OPERATIONAL);
+	state_machine.add_state(States::FALLO);
+	DigitalOutput pin(PB0);
+	auto ledtoggle = [&pin](){
+		pin.toggle();
+	};
+	state_machine.add_low_precision_cyclic_action(ledtoggle, 100ms);
+
+	auto ledon = [&pin](){
+		pin.turn_on();
+	};
+	state_machine.add_enter_action(ledon, States::FALLO);
+	ProtectionManager::link_state_machine(state_machine,States::FALLO);
 	STLIB::start();
+	//add_protection(&current,Boundary<uint64_t,BELOW>(10));
+
+	test.add();
 
 	while(1) {
+		ProtectionManager::check_protections();
 		STLIB::update();
 	}
 }
---
+++ tools/build.py
@@ -0,0 +1,101 @@
+import os
+import argparse
+import subprocess
+import git
+from colorama import Fore
+parser = argparse.ArgumentParser(prog="ConfigBuild",
+                                 description="Configures and builds the project")
+
+
+parser.add_argument('-bb','--build_behaviour',choices=['Release','Debug'],required=True)
+parser.add_argument('-target','--target',choices=['NUCLEO','BOARD'],required=True)
+parser.add_argument('-eth','--ethernet_config',choices=['ON','OFF'],required=True)
+parser.add_argument('-dir','--output_dir',required=False)
+parser.add_argument('-f','--flash',choices=['TRUE','FALSE'],default=False,required=False)
+
+args = parser.parse_args()
+
+class ConfigBuild:
+    def find_repo_root(self):
+        current_directory = os.getcwd()
+        repo = git.Repo(current_directory, search_parent_directories=True)
+        git_root = repo.git.rev_parse("--show-toplevel")
+        self.repo_root = git_root
+    def __init__(self):
+        self.buildconfig = args.build_behaviour
+        self.target = args.target
+        self.ethernet = args.ethernet_config
+        self.flash = args.flash
+        if args.output_dir == None:
+            self.output_dir = self.buildconfig         
+        else:
+             self.output_dir = args.output_dir
+        self.find_repo_root()
+        self.printConfiguration()
+
+    def find_file(self,search_path):
+        for root, _, files in os.walk(search_path):
+            for file in files:
+                if file.endswith(".elf"):
+                    return file
+
+    def flash_target(self):
+        location = self.repo_root + "/build/" + self.output_dir +"/"
+        file_name = self.find_file(location)
+        elf_src = location + file_name
+        binary_target = file_name.strip(".elf") + ".bin"
+        print(Fore.GREEN + "Elf name:\t" + file_name)
+        print("Binary target:\t" +binary_target +"\n\n" + Fore.YELLOW)
+
+        ret = subprocess.call(["arm-none-eabi-objcopy","-O","binary", elf_src, binary_target ])
+        if ret != 0:
+            print(Fore.RED + "Error when calling arm-none-eabi-objcopy, check it is installed")
+            raise Exception()
+        print("Flashing the code to the target\n")
+        ret2 = subprocess.call(["st-flash","write",binary_target,"0x8000000"])
+        if ret2 != 0:
+            print(Fore.RED + "Error when flashing to the board, check st-flash is installed")
+            raise Exception()
+        else:
+            print("\nSuccessfully flashed the code to the target")
+        subprocess.call(["st-flash","reset"])
+        print(Fore.LIGHTMAGENTA_EX + "\tStarting program execution")
+
+    def build(self):
+        try:
+            os.makedirs(self.repo_root + "/build/" + self.output_dir)
+        except FileExistsError:
+            pass
+
+        output = self.repo_root + "/build/" + self.output_dir
+        if self.buildconfig == "Release":
+                if self.target == "BOARD":
+                    subprocess.call(["cmake", self.repo_root, "-B", output, "-DOPTIMIZATION=-O3","-DDEBUG_CONFIGURATION=-g0","-DNUCLEO=FALSE"])
+                else:
+                    subprocess.call(["cmake", self.repo_root, "-B", output, "-DOPTIMIZATION=-O3","-DDEBUG_CONFIGURATION=-g0", "-DNUCLEO=TRUE"])
+        else:
+            if self.target == "BOARD":
+                subprocess.call(["cmake", self.repo_root, "-B", output, "-DOPTIMIZATION=-Og", "-DDEBUG_CONFIGURATION=-g3", "-DNUCLEO=FALSE"])
+            else:
+                subprocess.call(["cmake", self.repo_root, "-B", output, "-DOPTIMIZATION=-Og", "-DDEBUG_CONFIGURATION=-g3", "-DNUCLEO=TRUE"])                              
+        threads = os.cpu_count()
+        print( Fore.BLUE +  "\n\nCalling make with {} threads\n\n".format(threads))
+        retval = subprocess.call(["make","-j",str(threads),"-C", output])
+        if retval != 0:
+            print(Fore.RED + "ERRORS OCCURED\n")
+            raise Exception("error invoking make")
+        print(Fore.GREEN + "\nBuild completed successfully!!\n" + Fore.YELLOW)
+        if self.flash:
+            self.flash_target()
+    def printConfiguration(self):
+        print(Fore.CYAN +"\n\tConfiguration used:\n")
+        print(Fore.CYAN +"\t\tBuild configuration: [DEBUG, RELEASE]: {} ".format(self.buildconfig).upper())
+        print(Fore.CYAN +"\t\tTarget [BOARD,NUCLEO]: {} ".format(self.target))
+        print(Fore.CYAN +"\t\tEthernet [ON,OFF]: {} \n\n".format(self.ethernet))          
+
+
+
+obj = ConfigBuild()
+obj.build()
+
+obj.printConfiguration()
