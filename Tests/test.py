de8f0e2b709a0d211d2d44891a39c6ff37e5982b
Cantonplas <dcancat@etsinf.upv.es>
2025-02-20 02:04:16 +0100

    Temp

--- Tests/test.py
+++ Tests/test.py
@@ -2,49 +2,35 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), "VirtualMCU", "src"))
 
 from runner import runner
-from vmcu.shared_memory import SharedMemory
-from vmcu.pin import Pinout, DigitalOut
-from vmcu.services.digital_out import DigitalOutService
-from vmcu.assertions import *
+import vmcu.assertions as assertions
+import vmcu.services.communications.FDCAN as FDCAN
+import vmcu.pin.pinout as Pinout
+import vmcu.shared_memory as SharedMemory
 
 
 @runner.test()
 def led_toggle():
-    TOGGLE_PERIOD = milliseconds(100 * 2)
-    ALLOWED_SLACK = milliseconds(5)
-
-    shm = SharedMemory("gpio__blinking_led", "state_machine__blinking_led")
-    led = DigitalOutService(shm, Pinout.PA1)
-
-    def led_turns_on():
-        nonlocal led
-        return led.get_pin_state() is DigitalOut.State.High
-
-    def led_turns_off():
-        nonlocal led
-        return led.get_pin_state() is DigitalOut.State.Low
-
-    #sync with board
-    completes(
-        wait_until_true(led_turns_on),
-        before=(TOGGLE_PERIOD / 2) + ALLOWED_SLACK,
-        msg="Sync fails"
-    )
-
-    for i in range(150):
-        completes(
-            wait_until_true(led_turns_off),
-            before=(TOGGLE_PERIOD / 2) + ALLOWED_SLACK,
-            after=(TOGGLE_PERIOD / 2) - ALLOWED_SLACK,
-            msg="turns off"
-        )
-        completes(
-            wait_until_true(led_turns_on),
-            before=(TOGGLE_PERIOD / 2) + ALLOWED_SLACK,
-            after=(TOGGLE_PERIOD / 2) - ALLOWED_SLACK,
-            msg="turns on"
-        )
-        print("toggle", i)
+    ip = "127.0.0.1" 
+    port = 6969
+    sendport = 7070
+    print("Starting test")
+    shm = SharedMemory("gpio_FDCAN_TEST","state_machine_FDCAN_TEST")
+    print("Shared memory created")
+    fdcan = FDCAN(Pinout.PD1,Pinout.PD0, shm)
+    print("FDCAN created")
+    fdcan.start(ip, port,sendport)
+    print("FDCAN started")
+    assertions.completes(fdcan.transmit(),(1,b"Hello1",FDCAN.DLC.BYTES_6), before=assertions.seconds(1), msg="FDCAN did not transmit")
+    class InvalidPacketError(assertions.TestException):
+        def __init__(self, message):
+            super().__init__("InvalidPacketError", message)
+    def PacketReceived():
+        packet = fdcan.read()
+        if packet.rx_data == b"Hello2" and packet.identifier == 2 and packet.data_length == FDCAN.DLC.BYTES_6:
+            return True
+        raise InvalidPacketError("Invalid packet received")
+    
+    assertions.completes(assertions.wait_until_true(PacketReceived), before=assertions.seconds(1), msg="FDCAN did not receive the expected packet")
     
 
 runner.run() # Runs the tests, do not delete!
