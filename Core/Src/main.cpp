ea718aae9d685d2f77836efcc2874d2dc8d3bb2b
Cantonplas <dcancat@etsinf.upv.es>
2025-02-20 02:05:37 +0100

    temp

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -10,16 +10,23 @@ int main(void) {
 #ifdef SIM_ON
     SharedMemory::start();
 #endif
-
-    DigitalOutput led_on(PA1);
+    int id = FDCAN::inscribe(FDCAN::fdcan1); 
+    std::string data = "Hello2";
     STLIB::start();
+    Time::register_low_precision_alarm(500,[&](){
+        FDCAN::transmit(id, 1, data.c_str(), FDCAN::DLC::BYTES_6);
+    });
+    FDCAN::Packet packet;
+    Time::register_low_precision_alarm(500,[&](){
+        FDCAN::read(id, &packet);
+    });
+    while(1){
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
-    Time::register_low_precision_alarm(100, [&]() { led_on.toggle(); });
-
-    while (1) {
         STLIB::update();
     }
 }
+
 
 void Error_Handler(void) {
     ErrorHandler("HAL error handler triggered");
