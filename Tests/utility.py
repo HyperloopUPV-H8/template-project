cc5d84c1a45024c41e41b9ab30c599f3afd9a581
jdmarmen <jdmarmen@etsinf.upv.es>
2025-02-24 02:57:47 +0100

    Added initial helper functions for testing

--- Tests/utility.py
+++ Tests/utility.py
@@ -1,5 +1,7 @@
 from enum import Enum
 from vmcu.shared_memory import SharedMemory
+from vmcu.services.communications.Socket import Socket
+from vmcu.services.communications.spi import SPISlave
 
 class Utility:
     
@@ -11,7 +13,6 @@ class Utility:
         Args:
             shm (SharedMemory): Instance of the SharedMemory
             num (int): Number of expected active state machines
-
         '''
         return (num == shm.get_state_machine_count())
 
@@ -23,7 +24,6 @@ class Utility:
         Args:
             shm (SharedMemory): Instance of the SharedMemory
             state (Enum): Expected state
-
         '''
         return (state.value == shm.get_state_machine_state(1))
 
@@ -35,11 +35,47 @@ class Utility:
         Args:
             shm (SharedMemory): Instance of the SharedMemory
             state (Enum): Expected state
-
         '''
         return (state.value == shm.get_state_machine_state(2))
     
     @staticmethod
-    def connect_gui_connection():
-        pass
+    def connect_control_station(gui_connection, lip, lport, rip, rport)->Socket:
+        '''
+        Function that simulates a connection to the control station
+        -----------------------------------------------------------------------------------------------
+        Args:
+            gui_connection (None): Instance of the Socket that is initially initialized to None
+            lip (str): Local IP
+            lport (str):  Local port
+            rip (str): Remote IP
+            rport (str): Remote port
+        '''
+        if(gui_connection is None):
+            gui_connection=Socket(lip, lport, rip, rport)
+            gui_connection.connect()
+            return gui_connection
+        else:
+            print("GUI connection already established!")
     
+    @staticmethod
+    def disconnect_control_station(gui_connection: Socket):
+        '''
+        Function that disconnects the simulated connection to the control station
+        -----------------------------------------------------------------------------------------------
+        Args:
+            gui_connection (Socket): Instance of the Socket that is already initialized as a Socket
+        '''
+        if(gui_connection is not None):
+            gui_connection.stop()
+            gui_connection=None
+            return gui_connection
+
+    @staticmethod
+    def check_control_station_connection(gui_connection: Socket)->bool:
+        '''
+        Function that checks if the simulated connection to the control station is still active
+        -----------------------------------------------------------------------------------------------
+        Args:
+            gui_connection (Socket): Instance of the Socket that is already initialized as a Socket
+        '''
+        return gui_connection.is_running()
