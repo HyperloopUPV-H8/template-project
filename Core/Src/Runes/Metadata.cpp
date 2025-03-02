9a10a2663a267b1cf210ee1edb586494707984f0
Gonzalo Sanchez <gonzalosmoya24@gmail.com>
2025-03-02 14:43:46 +0100

    remove old Metadata

--- Core/Src/Runes/Metadata.cpp
+++
@@ -1,9 +0,0 @@
-extern "C"{
-    const char DESCRIPTION[255]  __attribute__((section(".metadata_pool")))= 
-        "****************"  // placeholder for beggining
-        "20250302T120100"   // DateTime using ISO-8601 format
-        " "                 // alignment
-        "5bbe11c5"          // STLIB commit
-        "45eda319"          // ADJ commit
-        "12345678";          // Board commit
-}
