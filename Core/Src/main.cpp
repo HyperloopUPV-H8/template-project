b5c7b21be551d92b407097adc1f5528732d81c68
Gonzalo <58850783+g0nz4I0@users.noreply.github.com>
2025-03-05 12:09:47 +0100

    remove commented code as mentioned in PR comments

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -15,11 +15,6 @@ int main(void) {
     STLIB::start();
 
     Time::register_low_precision_alarm(5, [&]() { led_on.toggle(); 
-    //  auto tiempo_actual = std::chrono::system_clock::now();
-    // auto tiempo_ms = std::chrono::duration_cast<std::chrono::milliseconds>(tiempo_actual.time_since_epoch()).count();
-
-    // // Imprimir el tiempo en milisegundos
-    // std::cout << "Tiempo en milisegundos: " << tiempo_ms << " ms\n";
     });
 
     while (1) {
