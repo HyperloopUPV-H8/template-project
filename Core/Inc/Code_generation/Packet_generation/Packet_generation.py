9f64c44a64664d454df5e2e1041c08c6d74c6d61
Cantonplas <dcancat@etsinf.upv.es>
2025-03-06 22:47:50 +0100

    Moved all generation and split

---
+++ Core/Inc/Code_generation/Packet_generation/Packet_generation.py
@@ -0,0 +1,199 @@
+from Packet_generation.Packet_descriptions import *
+import json
+import os
+import jinja2
+
+def Generate_PacketDescription():
+    with open("Core/Inc/Code_generation/Packet_generation/JSON_ADE/boards.json") as f:
+        boards = json.load(f)
+    boards_name = []
+    for board in boards["boards"]:
+        
+        with open("Core/Inc/Code_generation/Packet_generation/JSON_ADE/" + (boards["boards"][board])) as f:
+            b = json.load(f)
+        board_instance = BoardDescription(board, b)
+        boards_name.append(board_instance.name)
+        globals()[board] = board_instance
+    
+    return boards_name
+        
+
+#--------------DataPackets.hpp generation---------------#
+
+def Get_data_context(board:BoardDescription):
+    def GenerateDataEnum(board:BoardDescription):
+        Enums = []
+        for packet in board.packets:
+            for packet_instance in board.packets[packet]:
+                if packet_instance.type != "order":
+                    for measurement in packet_instance.measurements:
+                        if hasattr(measurement, "enum"):
+                            Enums.append(measurement.enum)
+        return Enums
+    
+    
+    def GenerateDataPackets(board:BoardDescription):
+        Packets =[]
+        totaldata = []
+        for packet in board.packets:
+            for packet_instance in board.packets[packet]:
+                if packet_instance.type != "order":
+                    tempdata = ""
+                    for variable in packet_instance.variables:
+                        tempdata +=(str(variable) +",")
+                    if tempdata.endswith(","):
+                        tempdata = tempdata[:-1]  
+                    aux_packet = {"name": packet_instance.name, "data":tempdata , "id": packet_instance.id}
+                    Packets.append(aux_packet)
+                    for measurement in packet_instance.measurements:
+                        aux_data = {"type": measurement.type, "name": measurement.id}
+                        totaldata.append(aux_data)
+        
+        return Packets,totaldata
+    
+    packets,data = GenerateDataPackets(board)
+    context = {
+        "board": board.name,
+        "enums": GenerateDataEnum(board),
+        "packets" : packets,
+        "data": data,
+        "size": board.order_size,
+    }
+    return context
+
+def Generate_DataPackets_hpp(board_input:str):
+    board_instance = globals()[board_input]
+    if board_instance.data_size == 0:
+        if os.path.exists("Core/Inc/Communications/Packets/DataPackets.hpp"):
+            os.remove("Core/Inc/Communications/Packets/DataPackets.hpp")
+        return    
+  
+    env= jinja2.Environment(loader=jinja2.FileSystemLoader("Core/Inc/Code_generation/Packet_generation"))
+    template = env.get_template("DataTemplate.hpp")
+    context = Get_data_context(board_instance)
+
+    
+    with open("Core/Inc/Communications/Packets/DataPackets.hpp","w") as Output:
+        Output.write(template.render(context))
+            
+#--------------OrderPackets.hpp generation---------------#
+
+def Get_order_context(board:BoardDescription):
+    def GenerateOrderEnum(board:BoardDescription):
+        Enums = []
+        for packet in board.packets:
+            for packet_instance in board.packets[packet]:
+                if packet_instance.type == "order":
+                    for measurement in packet_instance.measurements:
+                        if hasattr(measurement, "enum"):
+                            Enums.append(measurement.enum)
+        return Enums
+    
+    
+    def GenerateOrderPackets(board:BoardDescription):
+        Packets =[]
+        totaldata = []
+        for packet in board.packets:
+            for packet_instance in board.packets[packet]:
+                if packet_instance.type == "order":
+                    tempdata = ""
+                    for variable in packet_instance.variables:
+                        tempdata +=(str(variable) +",")
+                    if tempdata.endswith(","):
+                        tempdata = tempdata[:-1]  
+                    aux_packet = {"name": packet_instance.name, "data":tempdata , "id": packet_instance.id}
+                    Packets.append(aux_packet)
+                    for measurement in packet_instance.measurements:
+                        aux_data = {"type": measurement.type, "name": measurement.id}
+                        totaldata.append(aux_data)
+        
+        return Packets,totaldata
+    
+    
+    packets,data = GenerateOrderPackets(board)
+    context = {
+        "board": board.name,
+        "enums": GenerateOrderEnum(board),
+        "packets" : packets,
+        "data": data,
+        "size": board.order_size,
+    }
+    return context
+
+def Generate_OrderPackets_hpp(board_input:str):
+    board_instance = globals()[board_input]
+    if board_instance.order_size == 0:
+        if os.path.exists("Core/Inc/Communications/Packets/OrderPackets.hpp"):
+            os.remove("Core/Inc/Communications/Packets/OrderPackets.hpp")
+        return    
+  
+    env= jinja2.Environment(loader=jinja2.FileSystemLoader("Core/Inc/Code_generation/Packet_generation"))
+    template = env.get_template("OrderTemplate.hpp")
+    context = Get_order_context(board_instance)
+
+    
+    with open("Core/Inc/Communications/Packets/OrderPackets.hpp","w") as Output:
+        Output.write(template.render(context))
+
+
+#--------------Protections.hpp generation---------------#
+
+def Generate_Protections_context(board:BoardDescription):
+    def Get_Bondaries(measurement:MeasurmentsDescription):
+        Boundaries = []
+        for i in {0,1}:
+            for j in {0,1}:
+                if measurement.protections.protections[i].Protectionvalue[j] is None:
+                    continue
+                temp_boundary= {"type": measurement.type, "Above_or_Below":measurement.protections.protections[i].ProtectionType, "value": measurement.protections.protections[i].Protectionvalue[j],"coma":"," }
+                Boundaries.append(temp_boundary)
+        
+        Boundaries[-1]["coma"] = ""
+        return Boundaries
+            
+
+    def Get_protection_packets(board:BoardDescription):
+        protections = []
+        for packet in board.packets:
+            for packet_instance in board.packets[packet]:
+                for measurement in packet_instance.measurements:
+                    if hasattr(measurement, "protections"):
+                        protections.append(measurement)
+        if len(protections) == 0:
+            return False
+        return protections
+    
+    
+    protection_packets = Get_protection_packets(board)
+    if protection_packets == False:
+        return False
+    protections=[]
+    data =[]
+    for measurement in protection_packets:
+        Boundaries = Get_Bondaries(measurement)
+        aux_protection = {"packet": measurement.id, "Boundaries": Boundaries}
+        aux_data = {"type": measurement.type, "name": measurement.id}
+        if aux_data not in data:
+            data.append(aux_data)
+        if aux_protection in protections:
+            continue
+        protections.append(aux_protection)
+    
+    context ={
+        "board": board.name,
+        "data": data,
+        "protections": protections 
+    }
+    return context
+
+def Generate_Protections_hpp(board_input:str):
+    board_instance = globals()[board_input]
+    env= jinja2.Environment(loader=jinja2.FileSystemLoader("Core/Inc/Code_generation/Packet_generation"))
+    template = env.get_template("ProtectionsTemplate.hpp")
+    context = Generate_Protections_context(board_instance)
+    if context == False:
+        if os.path.exists("Core/Inc/Communications/Packets/Protections.hpp"):
+            os.remove("Core/Inc/Communications/Packets/Protections.hpp")
+            return
+    with open("Core/Inc/Communications/Packets/Protections.hpp","w") as Output:
+        Output.write(template.render(context))
