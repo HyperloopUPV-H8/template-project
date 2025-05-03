320c1cdb717d54743ac0ea3b88b266fe4f40c113
Jorge Sáez <jorgeesg82@gmail.com>
2025-05-04 01:02:20 +0200

    Added st-lib reference

--- Core/Src/Runes/generated_metadata.cpp
+++ Core/Src/Runes/generated_metadata.cpp
@@ -5,11 +5,11 @@
 extern "C"{
     const char DESCRIPTION[255]  __attribute__((section(".metadata_pool")))= 
         "****************"  // placeholder for beggining
-        "20250503T215516"   // DateTime using ISO-8601 format
+        "20250503T220942"   // DateTime using ISO-8601 format
         " "                 // alignment
         "ce53a8ad"          // STLIB commit
         "--------"          // ADJ commit
-        "ca661ec6"          // Board commit
+        "334854de"          // Board commit
         // the '=' is used for unparsing
         ;
 }
