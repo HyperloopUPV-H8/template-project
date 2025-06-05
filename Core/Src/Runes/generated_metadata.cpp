aa69bb073f7ff249e51b2e9b4f6bc5692bf5a2a7
jdmarmen <jdmarmen@upv.edu.es>
2025-06-05 17:08:33 +0200

    Updated ST-LIB

--- Core/Src/Runes/generated_metadata.cpp
+++ Core/Src/Runes/generated_metadata.cpp
@@ -5,11 +5,11 @@
 extern "C"{
     const char DESCRIPTION[255]  __attribute__((section(".metadata_pool")))= 
         "****************"  // placeholder for beggining
-        "20250605T162639"   // DateTime using ISO-8601 format
+        "20250605T165128"   // DateTime using ISO-8601 format
         " "                 // alignment
         "77b804dd"          // STLIB commit
         "--------"          // ADJ commit
-        "5dfe099d"          // Board commit
+        "3a365135"          // Board commit
         // the '=' is used for unparsing
         ;
 }
