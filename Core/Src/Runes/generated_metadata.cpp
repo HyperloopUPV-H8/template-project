485b4fada019f62a960fd541a50a5643fdd3abd4
Cantonplas <dcancat@etsinf.upv.es>
2025-09-13 16:25:34 +0200

    Deleted generated code

--- Core/Src/Runes/generated_metadata.cpp
+++ Core/Src/Runes/generated_metadata.cpp
@@ -5,11 +5,11 @@
 extern "C"{
     const char DESCRIPTION[255]  __attribute__((section(".metadata_pool")))= 
         "****************"  // placeholder for beggining
-        "20250911T170758"   // DateTime using ISO-8601 format
+        "20250605T165128"   // DateTime using ISO-8601 format
         " "                 // alignment
-        "1af648a0"          // STLIB commit
+        "77b804dd"          // STLIB commit
         "--------"          // ADJ commit
-        "85c1e62c"          // Board commit
+        "3a365135"          // Board commit
         // the '=' is used for unparsing
         ;
 }
